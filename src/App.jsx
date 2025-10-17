802ea660bddbed7e81c8a6f72470122ff90111e4
Ritu <iamritukashyap@gmail.com>
2025-10-17 22:01:27 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import './Login.css'
 import './App.css'
 import backgroundImage from './assets/cash.jpg';
+import settingsImage from './assets/settings.png';
+import {BrowserRouter,Routes, Route, Link} from 'react-router-dom';
+import Home from './Home.jsx'
+import Ana from './Ana.jsx'
+import Trans from './Trans.jsx'
+import Bills from './Bills.jsx'
+import Split from './Split.jsx'
+import Invest from './Invest.jsx'
 
 function Inside() {
-  const [count, setCount] = useState(0)
+  const [signReg, setSignReg] = useState("Register Here");
+  const [heading, setHeading] = useState("Sign In");
+  const [buttonc, setButton] = useState("Login");
 
   return (
     <div>
@@ -13,21 +24,32 @@ function Inside() {
       <div className="welcome">
        <h3><b>Welcome!</b></h3>
        <p>First time?</p>
-       <button type="button">Register here</button>
+       <button type="button" onClick={()=>{
+        if(signReg=="Register Here")
+        {setSignReg("Sign In Here");
+        setHeading("Sign Up");
+        setButton("Register");
+        }
+        else{
+          setSignReg("Register Here");
+        setHeading("Sign In");
+        setButton("Login");
+        }
+       }}>{signReg}</button>
       </div>
-      
       <div className="login">
-       <h3>Sign in</h3>
+       <h3>{heading}</h3>
        <input type="text" placeholder="Username"></input>
        <input type="password" placeholder="Password"></input>
-       <button type="submit">Login</button>
-      </div>
+       <button type="submit">{buttonc}</button>
+    </div>
+      
     </div>
     </div>
   )
 }
 
-function App(){
+function Main(){
   return(
   <div class="container" style={{ 
             backgroundImage: `url(${backgroundImage})`,
@@ -38,4 +60,46 @@ function App(){
   </div>
   )
 }
+
+function App(){
+
+  return(
+    <>
+    <BrowserRouter>
+    <nav>
+    <div className="navbar">
+    <div className="home">
+    <img src={viteLogo}></img>
+    <Link className="link" to="/"><b>Home</b></Link>
+    </div>
+    <div className="things">
+      <div className="thing"><Link className="link" to="/analysis"><b>Analysis</b></Link></div>
+      <div className="thing"><Link className="link" to="/transactions"><b>Transactions</b></Link></div>
+      <div className="thing"><Link className="link" to="/bills-due"><b>Bills Due</b></Link></div>
+      <div className="thing"><Link className="link" to="/splitting"><b>Splitting Bills</b></Link></div>
+      <div className="thing"><Link className="link" to="/investments"><b>Investments</b></Link></div>
+    </div>
+    <div className="settings">
+      <img src={settingsImage}></img>
+      <Link className="link" to="/settings"><b>Settings</b></Link>
+    </div>
+    </div>
+    </nav>
+    
+    <div className="the-main-part">
+    <Routes>
+    <Route path="/" Component={Home}/>
+    <Route path="/analysis" Component={Ana}/>
+    <Route path="/transactions" Component={Trans}/>
+    <Route path="/bills-due" Component={Bills}/>
+    <Route path="/splitting" Component={Split}/>
+    <Route path="/investments" Component={Invest}/>
+    </Routes>
+    </div>
+    </BrowserRouter>
+    </>
+  )
+}
+
 export default App
+
