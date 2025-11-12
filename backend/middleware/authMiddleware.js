1e954e79b3f586a67b373167aa7cc200032b05f7
Roopa Sreedhar A <193498568+coderRoopa@users.noreply.github.com>
2025-11-12 20:15:03 +0530

    Add files via upload

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+module.exports = async function (req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ message: 'No token provided.' });
+  }
+  const token = authHeader.split(' ')[1];
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    
+    req.user = {      
+      userId: decoded.userId,
+      email:decoded.email       
+    };
+    console.log("Decoded token:", req.user);
+
+    next();
+  } catch (err) {
+    res.status(401).json({ message: 'Token invalid.' });
+  }
+};
